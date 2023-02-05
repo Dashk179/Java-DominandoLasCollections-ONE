4387855e976c9848870758892f20979bdd2865e3
Arturo Perez <dash_arturo@hotmail.com>
2023-02-05 14:28:55 -0600

    Referencia y encapsulamineto de un objeto con un parametro List

--- src/Referencia_Y_Encapsulamiento_List.java
+++ src/Referencia_Y_Encapsulamiento_List.java
@@ -2,8 +2,9 @@ import model.Aula;
 import model.Curso;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class Objeto_Con_Parametro_List {
+public class Referencia_Y_Encapsulamiento_List {
     public static void main(String[] args) {
         //La ventaja de declarar un objeto List como parametro desde nuestra clase es que podemos
         //Ocupar las implementacines de List aunque no las declaremos al crear el objeto
@@ -16,9 +17,16 @@ public class Objeto_Con_Parametro_List {
         curso1.addAula(new Aula("List"));
         curso1.addAula(new Aula("LinkedList"));
 
+        List<Aula> aulaList = curso1.getAulaList();
+
+       // aulaList.add(new Aula("Inumtable")); <- Si esto funciona no deberia de ser asi, ya que estamos accediendo por medio de un metodo no encapsulado.
+        //Por lo cual tenemos que obligar a que solo se pueda modificar por medio del metodo encapsulado el cual es "addAula"
+        //Que tenemos que hacer para resolver este problema? Hacemos nuestra lista Inmutable (Lista no modificable).
+
+        curso1.addAula(new Aula("Inmutable"));
         ArrayList<Curso> cursos = new ArrayList<>();
         cursos.add(curso1);
 
-        System.out.println(cursos.get(0).getAulaList());
+        System.out.println(aulaList);
     }
 }
--- src/model/Curso.java
+++ src/model/Curso.java
@@ -1,22 +1,23 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Curso  {
     private String  nombre;
     private int tiempo;
-    private List<Clase> claseList = new ArrayList<>();
+    private List<Aula> aulaList = new ArrayList<>();
 
     public Curso(String nombre, int tiempo) {
         this.nombre = nombre;
         this.tiempo = tiempo;
     }
 
-    public Curso(String nombre, int tiempo,List<Clase> claseList) {
+    public Curso(String nombre, int tiempo,List<Aula> aulaList) {
         this.nombre = nombre;
         this.tiempo = tiempo;
-        this.claseList = claseList;
+        this.aulaList = aulaList;
     }
     public String getNombre() {
         return nombre;
@@ -34,17 +35,18 @@ public class Curso  {
         this.tiempo = tiempo;
     }
 
-    public List<Clase> getClaseList() {
-        return claseList;
+    public List<Aula> getAulaList() {
+        return Collections.unmodifiableList(aulaList); //Para que no sea modificada nuestra lista agregamos esta linea.
+        //Entonces estos nos obliga a utilizar nuestra clase encapsulada. -> addAula()
     }
 
-    public void setClaseList(List<Clase> claseList) {
-        this.claseList = claseList;
+    public void setAulaList(List<Aula> aulaList) {
+        this.aulaList = aulaList;
     }
 
-    public void addClase(Clase clase){
-        this.claseList.add(clase);
-    }
+    public void addAula(Aula aula){
+        this.aulaList.add(aula);
+    } //Clase encapsulada
     @Override
     public String toString() {
         return("Nombre: "  + this.nombre +" || "+" Duracion en hrs : " + this.tiempo +" || ");
